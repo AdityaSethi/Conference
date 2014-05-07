6a8d3874b5676a539db138f80cf4d73f1e5300b5
Aditya Sethi <aditya_sethi@external.mckinsey.com>
2014-05-07 11:45:16 +0530

    conference app bootstrapped

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,4 +1,10 @@
 angular.module('App.controllers', [])
+		.controller('ParentCtrl', ['$scope', function ($scope) {
+        $scope.currentTab = 'main';
+        $scope.selectTab = function(tab) {
+					$scope.currentTab = tab;
+    		};
+    }])
     .controller('MainCtrl', ['$scope', function ($scope) {
         $scope.status = "It works!";
     }])
