68eeb8733009de6a06ba9a64045b934630bc07fb
unknown <Rakesh Bandhu@FIR000074934001.ads.mckinsey.com>
2014-05-26 18:50:58 +0530

    Landing pages developed,working on speaker & agenda partials

--- www/scripts/controllers/main.js
+++ www/scripts/controllers/main.js
@@ -1,7 +1,18 @@
 'use strict';
 
 angular.module('conferenceApp')
-  .controller('MainCtrl', function ($scope, $location, $anchorScroll) {
-    $scope.status = "Main view"
+  .controller('MainCtrl', function ($scope, $location, $anchorScroll,$rootScope) {
+    $scope.status = "Main view";
+    $rootScope.deviceHeight=window.innerHeight;
+    $rootScope.deviceWidth=window.innerWidth;
 
+    $scope.changeOrientation=function()
+    {
+    	$rootScope.deviceWidth=window.innerWidth;
+    	$rootScope.deviceHeight=window.innerHeight;
+    }
+      window.addEventListener('orientationchange',$scope.changeOrientation());
+    
 });
+
+
--- www/scripts/controllers/parent.js
+++ www/scripts/controllers/parent.js
@@ -1,11 +1,15 @@
 'use strict';
 
 angular.module('conferenceApp')
-  .controller('ParentCtrl', function ($scope, $location) {
-    $scope.active_overview;
-    $scope.active_speaker;
-    $scope.active_agenda;
-    $scope.active_venue;
+  .controller('ParentCtrl', function ($scope, $location,$rootScope) {
+  	$scope.$watch
+  	{
+	    $scope.active_overview;
+	    $scope.active_speaker;
+	    $scope.active_agenda;
+	    $scope.active_venue;
+	    $scope.$apply;
+	}
      //    $scope.selectTab = function(tab) {
 					// $scope.currentTab = tab;
     	// 	};
