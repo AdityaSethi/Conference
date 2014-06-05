5859a66d20c7c7b0c9ee3c1f1842674611a3c467
hemalijain <hemalijains@gmail.com>
2014-06-05 18:03:15 +0530

    Maps marker removed and added Window

--- www/scripts/controllers/venue.js
+++ www/scripts/controllers/venue.js
@@ -1,13 +1,13 @@
 'use strict';
 
 angular.module('conferenceApp')
-.controller('venueCtrl',function($scope,$http) {
+.controller('venueCtrl',function($scope,$http,$rootScope) {
   	//$http.get('scripts/lib/json/venue.json').success(function(response) {
      //  }
     $scope.active_overview;
     $scope.active_speaker;
     $scope.active_agenda;
     $scope.active_venue="active";
-    $scope.address = "Hotel Taj Vivanta" + "\n" +"M G Road"+"\n"+"Bangalore"
-   
+    $scope.address = "Hotel Taj Vivanta" + "\n" +"M G Road"+"\n"+"Bengaluru"
+   	
   });
