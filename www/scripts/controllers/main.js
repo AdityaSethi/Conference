1775cfde64ada1ba66cd7c4787f504456bd28301
Aditya <aditya@Adityas-MacBook-Pro.local>
2015-06-21 10:31:45 +0530

    merging all jsons in one json, including speakers images

--- www/scripts/controllers/main.js
+++ www/scripts/controllers/main.js
@@ -12,22 +12,13 @@ angular.module('conferenceApp')
       $window.history.back();
     }
 
-    Data.get_data('scripts/lib/json/agenda.json').success(function(api_data) {
-    	SetData.set_agenda_data(api_data);
+    Data.get_data('scripts/lib/json/confData.json').success(function(api_data) {
+    	SetData.set_agenda_data(api_data.agenda);
+        SetData.set_overview_data(api_data.overview);
+        SetData.set_speaker_data(api_data.speakers);
+        SetData.set_venue_data(api_data.venue);
     });
 
-    Data.get_data('scripts/lib/json/overview.json').success(function(api_data) {
-    	SetData.set_overview_data(api_data);
-    });
-
-    Data.get_data('scripts/lib/json/speaker.json').success(function(api_data) {
-    	SetData.set_speaker_data(api_data);
-    });
-    
-    Data.get_data('scripts/lib/json/venue.json').success(function(api_data) {
-    	SetData.set_venue_data(api_data);
-    });
-    
     $rootScope.dayVal = 0;
 });
 
